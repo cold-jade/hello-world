8cd31d844018f1b88887bcea77ceded953553633
wangbo <1580345996@qq.com>
2018-06-07 11:06:12 +0800

    测试一下

--- routes/family-detail.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET family-detail page. */
-router.get('/', function(req, res, next) {
-  res.render('family-detail', {});
-});
-
-module.exports = router;
